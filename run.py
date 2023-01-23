191aaa4fc9411a9221e4ea03af2f42d0ce71feed
Zaurtime <zaurteam@gmail.com>
2023-01-23 09:33:42 +0000

    Add content for responses and add aditional comments

--- run.py
+++ run.py
@@ -1,64 +1,67 @@
 import re
 import long_responses as long
 
-def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
-     message_certainty = 0
-      has_required_words = True
 
-# Counts how many words are present in each predefined message
-for word in user_message:
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
-# Calculates the percent
-percentage = float(message_certainty) / float(len(recognised_words))
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
 
-#Check strings
-for word in required_words:
+    # Checks that the required words are in the string
+    for word in required_words:
         if word not in user_message:
             has_required_words = False
             break
 
- if has_required_words or single_response:
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
         return int(percentage * 100)
     else:
         return 0
 
+
 def check_all_messages(message):
     highest_prob_list = {}
 
-#Response creation / adds
- def response(bot_response, list_of_words, single_response=False, required_words=[]):
+    # Simplifies 
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
         nonlocal highest_prob_list
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
-response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
-response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
-response()
-response()
-response()
-response()
-response()
-response()
-response()
-response() 
+    
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+    response('It is rainy"!', ['weather', 'rain', 'windy'], required_words=['weather', 'rain'])
+    response('Code Institute', ['study', 'learn' , 'code' , 'project'], required_words=['study', 'project'])
+    response()
+    response()
+    response()
+    # Longer responses
+    response(long.R_ADVICE, ['give', 'advice', 'search'], required_words=['advice', 'search'])
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
 
-# Longer responses
-response()
-response()
-response()
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
 
-best_match = max(highest_prob_list, key=highest_prob_list.get)
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
-# print(highest_prob_list)
-
-return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
 # Used to get the response
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
     response = check_all_messages(split_message)
     return response
+
 
 # Testing the response system
 while True:
