8a1bf59cfb21558460490c756cf78109d4acae23
Zaurtime <zaurteam@gmail.com>
2023-01-23 06:29:14 +0000

    Add variable and boolean

--- run.py
+++ run.py
@@ -2,3 +2,5 @@ import re
 import long_responses as long
 
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+     message_certainty = 0
+      has_required_words = True
