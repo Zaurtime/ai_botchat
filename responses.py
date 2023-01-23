69e30628693a9308c61354b47a7b18c7ec3ba0fc
Zaurtime <zaurteam@gmail.com>
2023-01-23 06:25:19 +0000

    Add function

--- responses.py
+++
@@ -1,2 +0,0 @@
-import re
-import responses as responses
--- run.py
+++ run.py
@@ -1,1 +1,4 @@
 import re
+import long_responses as long
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
