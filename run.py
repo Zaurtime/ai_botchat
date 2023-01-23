fdaef6a35092579dba7c6480828fd258ed57b090
Zaurtime <zaurteam@gmail.com>
2023-01-23 07:19:56 +0000

    Add while true test response system

--- run.py
+++ run.py
@@ -59,3 +59,7 @@ def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
     response = check_all_messages(split_message)
     return response
+
+# Testing the response system
+while True:
+    print('Bot: ' + get_response(input('You: ')))
