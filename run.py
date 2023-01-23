9b26d8ea05ead698b3c02c88c2375718ee03ea5e
Zaurtime <zaurteam@gmail.com>
2023-01-23 06:35:00 +0000

    Add comment and loop

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ import long_responses as long
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
      message_certainty = 0
       has_required_words = True
+
+# Counts how many words are present in each predefined message
+for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
